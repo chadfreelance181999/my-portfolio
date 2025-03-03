a9d072566880e3087ac9583b4e7e8d03e2cf880e
Richard <richardjr@fullstack.ph>
2025-03-03 23:11:29 +0800

    Porttfolio

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,100 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>WordPress</li>
+        <li>Shopify</li>
+        <li>Jquery</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Next.js</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Udemy For Front End Development Course</li>
+        <li>(BSCS) Bachelor of Science in Computer Science at AMA University</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Philippine Society of Information Technology Educators (PSITE) Region 3</li>
+        <li>International Research Conference on Information Technology Education (IRCITE)</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image className="rounded-xl" src="/images/about.jpg" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+          I am deeply passionate about creating visually stunning, responsive, and
+           highly functional websites. With expertise in WordPress and Shopify
+          for dynamic content management, React and Next.js for building modern,
+          interactive interfaces, and HTML, CSS, and JavaScript for structuring
+          and styling web pages. I thrive on crafting digital experiences
+          that are both visually appealing and technically robust.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,126 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "T2DS",
+    description: "Design solution service provider",
+    image: "/images/projects/1.png",
+    tag: ["All", "WordPress"],
+    previewUrl: "https://www.t2ds.com/",
+  },
+  {
+    id: 2,
+    title: "ZEA Relief Cream",
+    description: "An E-commerce Website that Sells Pain Relief Cream",
+    image: "/images/projects/2.png",
+    tag: ["All", "Shopify"],
+    previewUrl: "https://www.zea.global/",
+  },
+  {
+    id: 3,
+    title: "Masseuse Massage",
+    description: "An E-commerce website that sells massage chair",
+    image: "/images/projects/3.png",
+    tag: ["All", "Shopify"],
+    previewUrl: "https://masseusemassage.com.au/",
+  },
+  {
+    id: 4,
+    title: "Product Board",
+    description: "A service provider on the start-up compony",
+    image: "/images/projects/4.png",
+    tag: ["All", "React"],
+    previewUrl: "https://www.productboard.com/",
+  },
+  {
+    id: 5,
+    title: "Double Jump",
+    description: "A WordPress website that provides Full Service Video Production or A La Carte Pricing",
+    image: "/images/projects/5.png",
+    tag: ["All", "WordPress"],
+    previewUrl: "https://www.doublejump.media/",
+  },
+  {
+    id: 6,
+    title: "AAR Autoglass",
+    description: "A WordPress website that deals a Autoglass product. ",
+    image: "/images/projects/6.png",
+    tag: ["All", "WordPress"],
+    previewUrl: "https://autoglassinsanantonio.com/",
+  },
+];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        Handled Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="WordPress"
+          isSelected={tag === "WordPress"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Shopify"
+          isSelected={tag === "Shopify"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="React"
+          isSelected={tag === "React"}
+        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ProjectsSection;
