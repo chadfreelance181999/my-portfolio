2d80bb29f518a667c9f5a0a81ca457b226565f87
chadfreelance181999 <richard.freelanceph@gmail.com>
2025-03-09 08:15:07 +0800

    Updated Heading Responsive

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-7 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h1 className="dark:text-white mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold">
+          <h1 className="dark:text-white mb-4 text-[28px] sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               Hello, Im{" "}
             </span>
